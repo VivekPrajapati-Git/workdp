744cea6ab862926445b17cacb7e4006aa8be12fc
VivekPrajapati-Git <vivekbusiness20@gmail.com>
2026-04-22 16:35:54 +0530

    Refractor : Made a single function that checks for n number of files

--- src/utils/file_exist_checker.py
+++ src/utils/file_exist_checker.py
@@ -1,8 +1,8 @@
 import click
 from pathlib import Path
 
-# Checking for one file exist or not
-def is_file_exist(file_name):
-    if Path.is_file(Path(file_name)) != True:
-        click.echo(f"File Error : {file_name} not found")
-        return        
+def is_file_exists(files):
+    for file in files:
+        if Path.is_file(Path(file)) != True:
+            click.echo(f"File Error : {file} not found")
+            return
